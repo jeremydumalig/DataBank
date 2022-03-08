48d2142032fb9a7b239c75f183ef26bd9fe7b97c
jeremydumalig <68886459+jeremydumalig@users.noreply.github.com>
2022-03-07 19:11:49 -0600

    Delete js directory

--- js/faculty.js
+++
@@ -1,7 +0,0 @@
-function splash() {
-    document.getElementById("gbaker").innerHTML = "<span onclick='swoosh()'>Streetball Nickname: G-Buckets</span>";
-}
-
-function swoosh() {
-    document.getElementById("gbaker").innerHTML = "<span onclick='splash()'>Faculty Advisor*</span>";
-}
